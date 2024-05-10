af1e3c5dda96c87e78942fa963a8c2427d9905e6
Siddharth Bajpai <siddharthbajpai007@protonmail.com>
2024-05-10 04:48:54 +0000

    first commit

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,28 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
+    if not use_salts :
+        with open("top-10000-passwords.txt",'r') as f:
+            line = f.readline()
+            while line != '':
+                if hashlib.sha1(line.encode()).hexdigest() == hash:
+                    f.close()
+                    return line
+            return "PASSWORD NOT IN DATABASE"
+    else:
+        with open("known-salts.txt",'r') as salts:
+            salt = salts.readline()
+            while salt != '':
+                with open("top-10000-passwords.txt",'r') as f:
+                    line = f.readline()
+                    while line != '':
+                        if hashlib.sha1(line.encode(),salt=salt.encode()).hexdigest() == hash:
+                            f.close()
+                            salts.close()
+                            return line
+                    f.close()
+            return "PASSWORD NOT IN DATABASE"
+                
+
+                
     return True
